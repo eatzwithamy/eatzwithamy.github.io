5809cc8d6710a1211986292476be1d7adfac87e9
eatzwithamy <40868377+eatzwithamy@users.noreply.github.com>
2018-07-05 11:35:26 -0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('#scrollbutton').click(function(){
+		$('html').animate({scrollTop : 0}, 300);
+	});
+
+	$('a[href]').click(function(){
+		$('html').animate({
+			scrollTop : $($.attr(this, 'href')).offset().top -170}, 300);
+	});
+});
